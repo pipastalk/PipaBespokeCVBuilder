b04a79805524a8f1e3c6b23c61f13ff2c3abcd71
Pipastalk <pipalabs@luckyfixes.co.uk>
2026-03-03 21:03:02 +0000

    Added Optional tags to many data properties

--- src/models/schema.py
+++ src/models/schema.py
@@ -1,77 +1,79 @@
 from dataclasses import dataclass
 from datetime import datetime
 from enum import Enum
+from typing import Optional
 
 @dataclass
 class Company:
     company_name: str
     industry: str
     size: int
-    culture: str
-    comment: str # personal notes about the company
+    culture: Optional[str]
+    comment: Optional[str] # personal notes about the company
 @dataclass
 class Skill:
     skill_name: str
     proficiency_level: str
-    related_placements: list["Placement"]
-    related_projects: list["Project"]
-    related_qualifications: list["Qualification"]
     enjoyment_level: int # how much do I enjoy this skill, could do with better name
     tags: list[str]
+    related_placements: Optional[list["Placement"]]
+    related_projects: Optional[list["Project"]]
+    related_qualifications: Optional[list["Qualification"]]
 @dataclass
 class Qualification:
     qualification_name: str
-    studied_at: str 
-    grade: str
+    studied_at: str
     awarded_date: datetime
-    expiration_date: datetime # for qualifications that expire, e.g. first aid, cpr, etc.
     related_skills: list["Skill"]
-    comment: str
     tags: list[str]
+    grade: Optional[str]
+    expiration_date: Optional[datetime] # for qualifications that expire, e.g. first aid, cpr, etc.
+    comment: Optional[str]
 @dataclass
 class ContactDetails:
-    email: str
-    phone_number: str
-    linkedin: str
-    github: str
-    other_links: list[str]
+    name: str
+    email: Optional[str]
+    phone_number: Optional[str]
+    linkedin: Optional[str]
+    github: Optional[str]
+    other_links: Optional[list[str]]
 @dataclass
 class Person:
     name: str
     contact_details: ContactDetails
     relation_type: str
-    comment: str
-    is_reference: bool
+    comment: Optional[str]
+    is_reference: Optional[bool]
 @dataclass
 class Project:
     project_name: str
     description: str
-    related_skills: list["Skill"]
     start_date: datetime
     end_date: datetime
-    comment: str
+    related_skills: Optional[list["Skill"]]
+    comment: Optional[str]
 @dataclass
 class Placement:
     company_details: Company
     job_title: str
     start_date: datetime
     end_date: datetime
-    project_references: list["Project"]
-    reference_contacts: list["Person"]
-    reason_for_leaving: str
-    related_skills: list["Skill"]
+    project_references: Optional[list["Project"]]
+    reference_contacts: Optional[list["Person"]]
+    related_skills: Optional[list["Skill"]]
+    reason_for_leaving: Optional[str]
 @dataclass
 class Location:
-    address: str #is there a library for this?
-    city: str
-    country: str #maybe use ISO country codes and a enum for this?
-    post_or_zip_code: str #check libraries for validating this?
-    gps_coordinates: str #TODO bonus extra for google maps link
+    city: str #Will accept Remote/Home as a city
+    address: Optional[str] #is there a library for this?
+    country: Optional[str] #maybe use ISO country codes and a enum for this?
+    post_or_zip_code: Optional[str] #check libraries for validating this?
+    gps_coordinates: Optional[str] #TODO bonus extra for google maps link
 @dataclass
 class AdvertSource:
     source_path: str #url or file path
     sourced_from: str #where this advert was found, e.g. linkedin, company website, etc.
-    comment: str #personal notes about the advert
+    comment: Optional[str] #personal notes about the advert
 @dataclass
 class Advert:
     advert_title: str
@@ -89,7 +91,7 @@ class Advert:
 class Hobby:
     hobby_name: str
     description: str
-    related_skills: list["Skill"]
-    awards_or_acolades: list[str]
     tags: list[str]
-    comment: str
+    related_skills: Optional[list["Skill"]]
+    awards_or_acolades: Optional[list[str]]
+    comment: Optional[str]
